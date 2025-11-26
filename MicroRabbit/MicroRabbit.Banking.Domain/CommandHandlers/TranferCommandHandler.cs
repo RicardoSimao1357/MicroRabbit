4fe0e8ec1d968f39f4402512e900a842a0161b0f
Ricardo <Ricardo.simao.1357@gmail.com>
2025-11-26 15:28:30 +0000

    Implemented Publish and SendCommand for Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/CommandHandlers/TranferCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TranferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+        public TranferCommandHandler(IEventBus bus)
+        {
+            _bus = bus;
+        }
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            // publish event to rabbitmq
+            _bus.PublishAsync(new TransferCreatedEvent(request.From, request.To, request.Amount));
+            return Task.FromResult(true);
+        }
+    }
+}
